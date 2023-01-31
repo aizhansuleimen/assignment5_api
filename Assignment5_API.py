4a07aa1822a7a766c589aa1c7e6078da02809099
aizhansuleimen <122606790+aizhansuleimen@users.noreply.github.com>
2023-01-31 11:27:17 +0100

    Add files via upload
    Initial upload

---
+++ Assignment5_API.py
@@ -0,0 +1,12 @@
+#Assignment5
+#importing packages
+import numpy as np
+import pandas as pd
+import requests
+import time
+from collections import ChainMap
+import warnings
+import geopandas as gpd
+
+import matplotlib
+import matplotlib.pyplot as plt
